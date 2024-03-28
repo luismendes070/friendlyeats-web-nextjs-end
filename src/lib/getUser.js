7fc717af5b5397aa31448b792d8e35e1f325d33f
Luis Mendes <machadoluismendes@gmail.com>
2024-03-28 07:30:11 -0300

    Increment
    npm install --save-dev @types/testing-library__jest-dom

--- src/lib/getUser.js
+++ src/lib/getUser.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react'
 import { auth } from '@/src/lib/firebase/firebase'
 import { useRouter } from 'next/navigation'
 
-import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import { auth } from '@/src/lib/firebase/firebase';
 import { useRouter } from 'next/navigation';
